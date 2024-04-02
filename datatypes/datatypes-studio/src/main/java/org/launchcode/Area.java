98f3305ad8db4aeb0a499cd0f10ecdaa2800e6ea
Travis <azai392@gmail.com>
2024-04-01 20:04:11 -0500

    Area done

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,18 @@
+package org.launchcode;
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+        double areaOfCircle = Math.PI * radius * radius;
+        System.out.println("The area of a circle of " + radius + " is: " + areaOfCircle);
+
+    }
+
+
+
+
+
+}

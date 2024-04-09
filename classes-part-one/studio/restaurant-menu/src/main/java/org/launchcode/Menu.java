1c44f6a203443659f25140f05d83843500cf6d5f
Travis <azai392@gmail.com>
2024-04-08 20:31:21 -0500

    Plan done

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,9 @@
+package org.launchcode;
+
+public class Menu {
+
+    public static void manu(String[] args) {
+
+
+    }
+}
---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -0,0 +1,34 @@
+package org.launchcode;
+
+
+public class MenuItem {
+
+
+
+/*
+"Item List" is assigned four things
+Lines 8-9 : four key value pairs. Print category as list title and other three as items in list
+> "name", "description", "price" = one item on list
+> "category" (from an array of three items) = name of the list
+Print "menu" get category [0,1,2] display category name, item values
+dessert -  appetizer - entrée
+
+Two classes - DONE
+
+Menu - main (runner)
+> Setters and Getters on Menu Items
+    - add item and remove item
+    - iterate over hashmap, select for category in order to print menu by category
+MenuItem
+> Setter and getters on Menu
+    - grab category for every one item, list name-description-price
+> methods constructors
+    - use hashmap to store/retrieve information for menu (Chapter 3)
+> String public name
+> String public description
+> Double public price
+> Use date class to determine if item was new.
+>
+ */
+
+}
